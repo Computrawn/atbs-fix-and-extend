3984e5ce8b485d2eec9e82b3efe41186d09a98de
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-05-18 15:21:48 -0500

    Copy previous code into project.

--- fix_and_extend.py
+++ fix_and_extend.py
@@ -12,3 +12,41 @@ logging.basicConfig(
     format="%(asctime)s -  %(levelname)s -  %(message)s",
 )
 logging.disable(logging.CRITICAL)  # Note out to enable logging.
+
+SQUARE_FIT_SIZE = 300
+LOGO_FILENAME = "new_cat_logo.png"
+
+logo_img = Image.open(LOGO_FILENAME)
+logo_width, logo_height = logo_img.size
+
+os.makedirs("with_logo", exist_ok=True)
+# Loop over all files in the working directory.
+for filename in os.listdir("."):
+    if (
+        not (filename.endswith(".png") or filename.endswith(".jpg"))
+        or filename == LOGO_FILENAME
+    ):
+        continue
+    img = Image.open(filename)
+    width, height = img.size
+
+    # Check if image needs to be resized.
+    if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
+        # Calculate the new width and hieght to be resized.
+        if width > height:
+            height = int((SQUARE_FIT_SIZE / width) * height)
+            width = SQUARE_FIT_SIZE
+        else:
+            width = int((SQUARE_FIT_SIZE / height) * width)
+            height = SQUARE_FIT_SIZE
+
+        # Resize the image.
+        print(f"Resizing {filename}")
+        img = img.resize((width, height))
+
+    # Add the logo.
+    print(f"Adding logo to {filename} ...")
+    img.paste(logo_img, (width - logo_width, height - logo_height), logo_img)
+
+    # Save changes.
+    img.save(os.path.join("with_logo", filename))
